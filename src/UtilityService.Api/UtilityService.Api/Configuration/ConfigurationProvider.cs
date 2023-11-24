c348c9cc93555766f1bd9efd414affaa3821bcaa
Aleksandr Khokhlachev <kho11@yandex.ru>
2023-11-24 22:06:41 +0500

    Init

---
+++ src/UtilityService.Api/UtilityService.Api/Configuration/ConfigurationProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace UtilityService.Api.Configuration;
+
+public class ApiConfig
+{
+	public string MongoDbConnectionString { get; set; }
+}
+
+public interface IConfigurationProvider
+{
+	ApiConfig GetConfig();
+	void SaveConfig(ApiConfig config);
+}
+
+public class ConfigurationProvider : IConfigurationProvider
+{
+	public ApiConfig GetConfig()
+	{
+		var config = new ApiConfig();
+
+		if (File.Exists("secrets.json"))
+		{
+			config = JsonSerializer.Deserialize<ApiConfig>(File.ReadAllText("secrets.json"));
+		}
+		else
+		{
+			SaveConfig(config);
+		}
+
+		if (string.IsNullOrEmpty(config?.MongoDbConnectionString))
+			throw new ArgumentException("Settings file not filled!");
+
+		return config;
+	}
+
+	public void SaveConfig(ApiConfig config)
+	{
+		File.WriteAllText("secrets.json", JsonSerializer.Serialize(config));
+	}
+}
